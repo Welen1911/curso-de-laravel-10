480173abf237e6cc46d4ae50f4be6c8de29d43d4
Welen1911 <welenalmeida2210@gmail.com>
2023-10-21 11:23:08 -0300

    Feat: header styled

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,37 +1,38 @@
-<h1>Listagem dos Suportes:</h1>
-<table>
-    <thead>
-        <th>Assunto</th>
-        <th>Descrição</th>
-        <th>Status</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ $support->body }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Detalhes</a>
-                </td>
-                <td>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
+@extends('admin.template.layout')
 
-</table>
+@section('title', 'Principal')
 
-<br>
+@section('header')
+    @include('admin.supports.partials.header', compact('supports'))
+@endsection
+
+@section('content')
+
+    <table>
+        <thead>
+            <th>Assunto</th>
+            <th>Descrição</th>
+            <th>Status</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>{{ getStatusSupport($support->status) }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}">Detalhes</a>
+                    </td>
+                    <td>
+                        <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+
+    </table>
 
 
-<a href="/supports/create">Cadastrar dúvida</a>
-
-<br>
-
-<x-pagination 
-    :paginator="$supports"
-    :appends="$filters"
-    />
+    <x-pagination :paginator="$supports" :appends="$filters" />
+@endsection
