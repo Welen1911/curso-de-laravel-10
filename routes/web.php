5b8aca8e4bca2b650cfd8972bb5f71ea3f7c267d
Welen1911 <welenalmeida2210@gmail.com>
2023-10-25 10:23:32 -0300

    Feat: auth with Breeze

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Enums\SupportStatus;
 use App\Http\Controllers\SupportController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\vaga\VagaController;
 use App\Models\Support;
 use Illuminate\Support\Facades\Route;
@@ -11,28 +12,28 @@ Route::get('/', function () {
     return redirect()->route('supports.index');
 });
 
+
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-
-
-
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-
-Route::post('/supports/create', [SupportController::class, 'store'])->name('supports.store');
-
-
-
 Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::put('/supports/update/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::get('/teste', [SupportController::class, 'create'])->name('supports.create');
+    Route::post('/supports/store', [SupportController::class, 'store'])->name('supports.store');
+
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::put('/supports/update/{id}', [SupportController::class, 'update'])->name('supports.update');
 
 
+    Route::delete('/supports/destroy/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+});
 
-Route::delete('/supports/destroy/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-
-
-
-Route::get('/vagas', [VagaController::class, 'showAll'])->name('vagas');
+require __DIR__ . '/auth.php';
